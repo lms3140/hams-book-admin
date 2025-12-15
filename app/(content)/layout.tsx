f7b244af4e28e4a2f706a6d35bee41dc212b81a0
Pru <korea314044@gmail.com>
2025-12-15 12:30:27 +0900

    feat: fetch wrapper 함수 구현

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,5 +1,13 @@
+import { cookies } from "next/headers";
 import DefaultLayout from "../_components/DefaultLayout";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  console.log(cookieStore.get("accessToken"));
+
   return <DefaultLayout>{children}</DefaultLayout>;
 }
---
+++ app/_lib/api/server/fetch.ts
@@ -0,0 +1,38 @@
+export const getServerFetch = async (url: string, token?: string) => {
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    cache: "force-cache",
+    next: { revalidate: 60 },
+  });
+  if (!resp.ok) {
+    throw new Error(`${resp.status}`);
+  }
+  const data = await resp.json();
+  return data;
+};
+
+export const postServerFetch = async <T>(
+  url: string,
+  body: T,
+  token?: string
+) => {
+  const resp = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    body: JSON.stringify(body),
+    cache: "no-store",
+  });
+
+  if (!resp.ok) {
+    throw new Error(`(${resp.status}) ${resp.statusText}`);
+  }
+  const data = await resp.json();
+  return data;
+};
