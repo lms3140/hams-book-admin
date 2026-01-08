96fcafd76e1be5dfe0d35a3ed11d64167e8445ac
Pru <korea314044@gmail.com>
2026-01-09 04:37:07 +0900

    deploy: 배포 2차 시도

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,12 +1,17 @@
-import { cookies } from "next/headers";
+import AuthComponent from "../_components/Auth/AuthComponent";
 import DefaultLayout from "../_components/DefaultLayout";
 import { SERVER_URL } from "../_lib/api/common/config";
-import { request } from "http";
+import { getServerFetch } from "../_lib/api/server/fetch";
 
 export default async function layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DefaultLayout>{children}</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <>{children}</>
+      <AuthComponent />
+    </DefaultLayout>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,10 @@
 import Link from "next/link";
+import LoginForm from "./_components/Auth/LoginForm";
 
 export default async function Home() {
   return (
-    <div className="w-lvw h-lvh flex justify-center items-center">
-      <div className="flex gap-4">
-        <Link
-          className="text-center border-3 border-cyan-200 rounded-4xl p-2 w-28 bg-cyan-100 hover:bg-cyan-200"
-          href={"/login"}
-        >
-          로그인
-        </Link>
-        <Link
-          className="text-center border-3 border-cyan-200 rounded-4xl p-2 w-28 bg-cyan-100 hover:bg-cyan-200"
-          href={"/order"}
-        >
-          주문
-        </Link>
-        <Link
-          className="text-center border-3 border-cyan-200 rounded-4xl p-2 w-28 bg-cyan-100 hover:bg-cyan-200"
-          href={"/user"}
-        >
-          유저
-        </Link>
-        <Link
-          className="text-center border-3 border-cyan-200 rounded-4xl p-2 w-28 bg-cyan-100 hover:bg-cyan-200"
-          href={"/book"}
-        >
-          책
-        </Link>
-      </div>
+    <div>
+      <LoginForm />
     </div>
   );
 }
