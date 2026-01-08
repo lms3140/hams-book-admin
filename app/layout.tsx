dbc9cc4147eed85eff76b7d7a87ce2cee79d8963
Pru <korea314044@gmail.com>
2026-01-09 04:53:55 +0900

    deploy:썸네일 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "햄스문고 - 관리자 페이지",
   description: "햄스문고 관리자 페이지입니다.",
+  openGraph: {
+    title: "햄스문고",
+    description: "햄스문고 관리자 페이지!",
+    url: "https://hams-book-admin.vercel.app/",
+    images: [
+      {
+        url: "https://hams-book-admin.vercel.app/t2.png",
+        width: 1200,
+        height: 630,
+        alt: "햄스문고 관리자 썸네일",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
