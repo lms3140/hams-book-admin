2ae3b2a2ac44b57556d3b195c675d539390be48f
Pru <korea314044@gmail.com>
2026-01-13 14:48:38 +0900

    fix: log 삭제

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -2,8 +2,6 @@ import { cookies } from "next/headers";
 import LoginForm from "../../_components/Auth/LoginForm";
 
 export default async function LoginPage() {
-  const cookieStore = (await cookies()).get("accessToken");
-
   return (
     <div>
       <LoginForm />
